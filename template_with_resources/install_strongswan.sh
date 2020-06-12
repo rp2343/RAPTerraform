2e989a2137847b2004d439a05e7f86730a8cf8e5
Rakesh Patil <rap@microsoft.com>
2020-06-12 17:08:30 +0530

    Adding custom extension script

---
+++ template_with_resources/install_strongswan.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum update -y
+yum install -y strongswan
+cd /usr/bin
+sudo wget http://www.vdberg.org/~richard/tcpping
+chmod 755 tcpping
+systemctl enable strongswan
+leftsubnet=$(ip route list |grep -i "/" | awk -F " " '{print $1}')
+cat << EOF > /etc/strongswan/ipsec.conf
+config setup
+        # strictcrlpolicy=yes
+        uniqueids = yes
+
+conn host-to-host
+    ikelifetime=60m
+    keylife=20m
+    rekeymargin=3m
+    keyingtries=1
+
+conn trap-any
+    also=host-to-host
+    right=%any
+    leftsubnet=$leftsubnet
+#    rightsubnet=10.1.2.0/24
+    rightsubnet=%any
+    type=transport
+    authby=psk
+    auto=route
+EOF
+systemctl start strongswan
+
+secret=$(openssl rand -base64 24)
+
+cat << EOF >> /etc/strongswan/ipsec.secrets
+: PSK "$secret"
+EOF
